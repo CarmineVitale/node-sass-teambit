d6fe35a88a800ae81890482f60afdc8b5bdc7218
CarmineVitale <carmine.vitale90@gmail.com>
2020-05-13 14:15:33 +0200

    Organizzazione lavoro

---
+++ dist/js/main.js
@@ -0,0 +1,24 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	/************************************************************************/
+(() => {
+/*!************************!*\
+  !*** ./src/js/main.js ***!
+  \************************/
+/*! unknown exports (runtime-defined) */
+/*! exports [maybe provided (runtime-defined)] [unused] */
+/*! runtime requirements:  */
+console.log('js ready');
+})();
+
+(() => {
+/*!*****************************!*\
+  !*** ./src/scss/style.scss ***!
+  \*****************************/
+/*! unknown exports (runtime-defined) */
+/*! exports [maybe provided (runtime-defined)] [unused] */
+/*! runtime requirements:  */
+// extracted by mini-css-extract-plugin
+})();
+
+/******/ })()
+;
