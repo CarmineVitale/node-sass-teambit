0163cc0228538d30114fc740607f858e77cc1457
CarmineVitale <carmine.vitale90@gmail.com>
2020-05-14 18:07:45 +0200

    Aggiunto dropdown menu in header responsive (da rivedere completamente)

--- dist/js/main.js
+++ dist/js/main.js
@@ -7,7 +7,12 @@
 /*! unknown exports (runtime-defined) */
 /*! exports [maybe provided (runtime-defined)] [unused] */
 /*! runtime requirements:  */
-console.log('js ready');
+$(document).ready(function () {
+  $('i.fa-bars').click(function () {
+    $(this).toggleClass('fa-times');
+    $('.respmenu ul').toggleClass('active');
+  }); // end click
+}); //end doc ready
 })();
 
 (() => {
--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,7 @@
-console.log('js ready');
+$(document).ready(function () {
+    $('i.fa-bars').click(function() { 
+       $(this).toggleClass('fa-times');
+       $('.respmenu ul').toggleClass('active');
+    }); // end click
+    
+}); //end doc ready
